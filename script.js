cf49c63647593e3ae2e98956bd0a617f38f11f09
Abhishek <abhi102509@gmail.com>
2023-12-02 20:09:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function removeBackground() {
+    var fileInput = document.getElementById("file");
+    var file = fileInput.files[0];
+    var formData = new FormData();
+    formData.append("image_file", file);
+    fetch("https://api.remove.bg/v1.0/removebg", {
+      method: "POST",
+      headers: {
+        "X-Api-Key": "UAhPS6Gc5GUaFtaeJThRSKXm"
+        // "X-Api-Key": "58H3zJsVo6Utbax17bfBiD7G"
+      },
+      body: formData
+    })
+    .then(response => response.blob())
+    .then(blob => {
+      var url = URL.createObjectURL(blob);
+      var resultDiv = document.getElementById("result");
+      var image = new Image();
+      image.src = url;
+      resultDiv.appendChild(image);
+  
+      // Create download link
+      var link = document.createElement("a");
+    link.href = url;
+    link.download = "background_removed.png";
+    var span = document.createElement("span");
+    span.innerHTML = "Download <i class='fa fa-download'></i>";
+    link.appendChild(span);
+    resultDiv.appendChild(link);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+  
+  
